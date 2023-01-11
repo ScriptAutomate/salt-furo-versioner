2d6516ad34c48c1b841d4fdc4a29078458634072
Wayne Werner <wwerner@vmware.com>
2023-01-11 17:14:29 -0600

    add url_prefix

--- src/salt_furo_versioner/__init__.py
+++ src/salt_furo_versioner/__init__.py
@@ -3,17 +3,17 @@ from collections import namedtuple
 Version = namedtuple("Version", "name,url")
 
 
-def make_html_context(*, current_release, latest_version, versions):
+def make_html_context(*, current_release, latest_version, versions, url_prefix="./"):
     if isinstance(versions, str):
         raise TypeError(
             f"other_versions {other_versions} was str, not list. Probably not what you wanted."
         )
     version_names = versions
-    latest_version = Version(name=latest_version, url=f"./{latest_version}")
-    current_version = Version(name=current_release, url=f"./{current_release}")
+    latest_version = Version(name=latest_version, url=f"{url_prefix}{latest_version}/")
+    current_version = Version(name=current_release, url=f"{url_prefix}{current_release}/")
 
     versions = [
-        Version(name=f"{version}", url=f"./{version}") for version in version_names
+        Version(name=f"{version}", url=f"{url_prefix}{version}/") for version in version_names
     ]
 
     html_context = {
